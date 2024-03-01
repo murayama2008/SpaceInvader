abb20fc31210d63e02d129df8afc5133ebd1c288
murayama2008 <161662767+murayama2008@users.noreply.github.com>
2024-03-01 10:39:31 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1709263765,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/fighter-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/bulleteffect-sheet0.png",
+		"images/ufo-sheet0.png",
+		"images/particles.png",
+		"images/sprite2-sheet0.png",
+		"images/berdarah-sheet0.png",
+		"images/gameover-sheet0.png",
+		"images/gameover3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
